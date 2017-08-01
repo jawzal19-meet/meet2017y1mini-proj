8ec39cfb15967f2368dd5c3642d87be864215da4
jawzal19-meet <jawzal19@meet.mit.edu>
2017-08-01 16:58:10 +0300

    Stopping here

--- snake.py
+++ snake.py
@@ -1,32 +1,44 @@
 def up():
     global direction
     direction=UP
-    move_snake()
     print('you pressed the up key!')
 
 def left():
     global direction
     direction=LEFT
-    move_snake()
     print('you pressed the left key!')
     
 def down():
     global direction
     direction=DOWN
-    move_snake()
     print('you pressed the down key!')
     
 def right():
     global direction
     direction=RIGHT
-    move_snake()
     print('you pressed the right key!') 
 
 def move_snake():
     my_pos= snake.pos()
     x_pos= my_pos[0]
     y_pos= my_pos[1]
+    new_pos=snake.pos()
+    new_x_pos=new_pos[0]
+    new_y_pos=new_pos[1]
 
+    if new_x_pos>=RIGHT_EDGE:
+        print('you hit the right edge! Game over!')
+        quit()
+    elif new_y_pos<=DOWN_EDGE:
+         print('you hit the bottom edge! Game over!')
+         quit()
+    elif new_x_pos<=LEFT_EDGE:
+          print('you hot the life edge! Game over!')
+          quit()
+    elif new_y_pos>=UP_EDGE:
+         print('you hit the top edge! Game over!')
+         quit()
+        
     if direction==RIGHT:
         snake.goto(x_pos + SQUARE_SIZE, y_pos)
         print("you moved right!")
@@ -46,7 +58,17 @@ def move_snake():
     stamp_list.append(new_stamp)
     old_stamp=stamp_list.pop(0)
     snake.clearstamp(old_stamp)
+    global food_stamps, food_pos
+    if snake.pos() in food_pos:
+        food_ind=food_pos.index(snake.pos())
+        food.clearstamp(food_stamps[food_ind])
+        food_pos.pop(food_ind)
+        food_stamps.pop(food_ind)
+        print("You have eaten the food!")
+        
     pos_list.pop(0)
+    turtle.ontimer(move_snake,TIME_STEP)
+ 
     
 ########################################3
 
@@ -95,15 +117,26 @@ DOWN=2
 RIGHT=3
 
 direction=UP
+UP_EDGE=250
+DOWN_EDGE=-250
+RIGHT_EDGE=400
+LEFT_EDGE=-400
 turtle.onkeypress(up, UP_ARROW)
 turtle.onkeypress(left,LEFT_ARROW)
 turtle.onkeypress(down,DOWN_ARROW)
 turtle.onkeypress(right,RIGHT_ARROW)
-
-
-
-
-
-
-
 turtle.listen()
+move_snake()
+#turtle.register_shape("trash.gif")
+food= turtle.clone()
+food.shape("turtle")
+food_pos=[(100,100), (-100,100), (-100,-100), (100,-100)]
+food_stamp=[]
+for this_food_pos in food_pos:
+    food.goto(this_food_pos)
+    new_food=food.stamp()
+    food_stamp.append(new_food)
+
+
+
+turtle.mainloop()
